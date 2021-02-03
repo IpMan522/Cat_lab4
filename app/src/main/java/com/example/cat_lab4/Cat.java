b80431af3f75ebb797e65457dda13aae7c73a68c
IpMan522 <mihailanatolevic844@gmail.com>
2021-02-03 23:05:15 +0700

    Добавил View и закрасил его в цвет кота

--- app/src/main/java/com/example/cat_lab4/Cat.java
+++ app/src/main/java/com/example/cat_lab4/Cat.java
@@ -2,6 +2,7 @@ package com.example.cat_lab4;
 
 public class Cat {
 
+
     public String name; // имя
     public int age; // возраст
     public int color; // цвет
--- app/src/main/java/com/example/cat_lab4/MainActivity.java
+++ app/src/main/java/com/example/cat_lab4/MainActivity.java
@@ -5,24 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView catDedscription;
+    View colorBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        catDedscription = findViewById(R.id.catDescr);
+        colorBox = findViewById(R.id.colorBox);
 
         Cat murzik = new Cat();
         murzik.name = "Мурзик";
-        murzik.age = 9;
-        murzik.color = Color.BLACK;
+        murzik.age = 7;
+        murzik.color = Color.YELLOW;
+
+        String output = "";
 
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        Log.i("GSON", gson.toJson(murzik));
+        String json = gson.toJson(murzik);
+
+        Cat murzik2 = new Cat();
+        murzik2 = gson.fromJson(json, Cat.class);
+
+        output = "Кот: " + murzik2.name +", " + murzik2.age + " лет, цвет:" ;
+        catDedscription.setText(output);
+        colorBox.setBackgroundColor(murzik2.color);
+
     }
 }
